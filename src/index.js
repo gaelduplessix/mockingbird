07660242700b17e2d4aa05e75af19fd1d82fc48d
Gael du Plessix <gael.duplessix@etixgroup.com>
2016-12-25 12:34:17 +0100

    WIP: Register a service worker that delegates fetch responses to the page

---
+++ src/index.js
@@ -0,0 +1,64 @@
+
+// Install service worker
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('/serviceWorker.js').then((registration) => {
+    // Registration was successful
+    onReady()
+  }).catch((err) => {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err)
+  })
+})
+
+let workerChannel
+
+function onReady() {
+  // Create a message channel to communicate with the worker
+  workerChannel = new MessageChannel()
+  workerChannel.port1.onmessage = (event) => {
+    onWorkerMessage(event)
+  }
+
+  // Send a message to the worker so it can uses the channel
+  navigator.serviceWorker.controller.postMessage({
+    type: 'OPEN_CHANNEL',
+  }, [workerChannel.port2])
+}
+
+function sendMessage(message) {
+  workerChannel.port1.postMessage(message)
+}
+
+function onWorkerMessage(event) {
+  console.log('message from worker:', event)
+  if (!event.data.type) {
+    return
+  }
+
+  const payload = event.data.payload
+
+  switch (event.data.type) {
+    case 'NEW_REQUEST':
+      console.log('new pending request', payload)
+      break;
+    default:
+      // Ignore message
+  }
+
+}
+
+function requestResponse(requestId, body, params) {
+  sendMessage({
+    type: 'REQUEST_RESPONSE',
+    payload: { requestId, body, params },
+  })
+}
+
+setTimeout(() => {
+  // Perform a fetch
+  fetch('/api/users').then((response) => {
+    console.log('sucess!', response)
+  }, (error) => {
+    console.log('error...', error)
+  })
+}, 500)
---
+++ src/serviceWorker.js
@@ -0,0 +1,44 @@
+console.log('Worker launched!')
+
+const pendingRequests = {}
+let messageChannel = null
+
+self.addEventListener('fetch', (event) => {
+  if (messageChannel && event.request.url.match(/api/)) {
+    const requestId = Math.random()
+    event.respondWith(new Promise((resolve, reject) => {
+      pendingRequests[requestId] = { event, resolve, reject }
+      messageChannel.postMessage({
+        type: 'NEW_REQUEST',
+        payload: {
+          id: requestId,
+          url: event.request.url,
+        },
+      })
+    }))
+  } else {
+    event.respondWith(fetch(event.request))
+  }
+})
+
+self.addEventListener('message', (event) => {
+  console.log('Message channel opened in worker')
+  messageChannel = event.ports[0]
+  event.ports[0].onmessage = (event) => {
+    console.log('message received in worker', event)
+    if (!event.data.type) {
+      return
+    }
+
+    const payload = event.data.payload
+
+    switch (event.data.type) {
+      case 'REQUEST_RESPONSE':
+        const requestId = payload.requestId
+        pendingRequests[requestId].resolve(new Response(payload.body, payload.params))
+        break;
+      default:
+        // Ignore message
+    }
+  }
+})
